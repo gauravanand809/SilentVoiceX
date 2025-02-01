d3dec5b280da87b50d9032e8842d16b73fa8fb66
gaurav <gaurav.anand.che22@iitbhu.ac.in>
2025-02-01 14:45:30 +0530

    auth changes

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -17,20 +17,24 @@ export async function GET(request: Request) {
     );
   }
   const userId = new mongoose.Types.ObjectId(_user._id);
+  // console.log(userId);
   try {
-    const user = await UserModel.aggregate([
+    const find_user = await UserModel.findById({_id:userId}) || "";
+    // console.log(find_user)
+     const user = await UserModel.aggregate([
       { $match: { _id: userId } },
       { $unwind: '$messages' },
       { $sort: { 'messages.createdAt': -1 } },
       { $group: { _id: '$_id', messages: { $push: '$messages' } } },
     ]).exec();
+    // console.log(_user);
+    // if (!user || user.length === 0) {
+    //   return Response.json(
+    //     { message: 'User not found', success: false },
+    //     { status: 404 }
+    //   );
+    // }
 
-    if (!user || user.length === 0) {
-      return Response.json(
-        { message: 'User not found', success: false },
-        { status: 404 }
-      );
-    }
 
     return Response.json(
       { messages: user[0].messages },
